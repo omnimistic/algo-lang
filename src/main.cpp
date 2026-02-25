85490caaf872a48746ad55aa952eb32ce7d0ef08
omnimistic <prasparadise@gmail.com>
2026-02-26 01:37:37 +0530

    Added IF keyword and shifted entire code execution part to a seperate function inorder for IF to do recursion to execute condition met action

--- src/main.cpp
+++ src/main.cpp
@@ -40,6 +40,16 @@ bool checkVarExists(const string& varName) {
     return true;
 }
 
+//helper function to evaluate a raw number or pull a variable's value for the IF statement
+float getValue(const string& token) {
+    if (isdigit(token[0]) || (token[0] == '-' && token.length() > 1 && isdigit(token[1]))) {
+        return stof(token);
+    } else {
+        if (checkVarExists(token)) return variables[token].f_val;
+        return 0.0f; 
+    }
+}
+
 
 float doMaths(const string& expression) {
 
@@ -144,6 +154,172 @@ float doMaths(const string& expression) {
     return p.parseExpression(expression, pos);
 }
 
+//engine that processes a single line, moved here so IF can call it recursively
+void executeLine(string line, int& i, const vector<string>& Buffer) {
+    stringstream ss(line);
+    string command;
+    ss >> command;
+
+    if (command == "END") {
+        isRunning = false; //strop executing
+        return;
+    }
+    else if (command == "DECLARE") {
+        string restOfLine;
+        getline(ss, restOfLine);
+        stringstream declStream(restOfLine);
+        string varName;
+        while (getline(declStream, varName, ',')) {
+            varName = trim(varName);
+            if (!varName.empty()) {
+                if (variables.count(varName) > 0) {
+                    cout << "Error: Variable '" << varName << "' already declared!" << endl;
+                } else {
+                    variables[varName] = varValue();
+                }
+            }
+        }
+    }
+    else if (command == "INPUT") {
+        string varName;
+        ss >> varName;
+        
+        if (!checkVarExists(varName)) return;
+
+        cout << "Enter value for " << varName << ": ";
+        string rawInput;
+        getline(cin >> ws, rawInput);
+        
+        varValue newVal;
+
+        //checks if starts with quote for string
+        if (rawInput[0] == '"') {
+            newVal.type = TYPE_STRING;
+            size_t lastQuote = rawInput.find_last_of('"');
+            if (lastQuote != string::npos && lastQuote > 0) {
+                newVal.s_val = rawInput.substr(1, lastQuote - 1);
+            } else {
+                newVal.s_val = rawInput.substr(1);
+            }
+        }
+        //checks if it has a dot for float
+        else if (rawInput.find('.') != string::npos) {
+            newVal.type = TYPE_FLOAT;
+            try { newVal.f_val = stof(rawInput); } catch(...) { newVal.f_val = 0.0f; }
+            newVal.i_val = (int)newVal.f_val; 
+        }
+        //otherwise assumes its an integer
+        else {
+            newVal.type = TYPE_INT;
+            try { newVal.i_val = stoi(rawInput); } catch(...) { newVal.i_val = 0; }
+            newVal.f_val = (float)newVal.i_val; 
+        }
+        
+        //save it to map
+        variables[varName] = newVal;
+    }
+    else if (command == "OUTPUT") {
+        string restOfLine;
+        getline(ss, restOfLine);
+        restOfLine = trim(restOfLine); 
+
+        if (restOfLine.empty()) return;
+
+        //if string literal
+        if (restOfLine[0] == '"') {
+            size_t lastQuote = restOfLine.find_last_of('"');
+            if (lastQuote != string::npos && lastQuote > 0) {
+                cout << ">> " << restOfLine.substr(1, lastQuote - 1) << endl;
+            } else {
+                cout << "Error: Missing closing quote in OUTPUT!" << endl;
+            }
+        }
+        //if raw number
+        else if (isdigit(restOfLine[0]) || (restOfLine[0] == '-' && restOfLine.length() > 1 && isdigit(restOfLine[1]))) {
+            cout << ">> " << restOfLine << endl;
+        }
+        //else must be a variable
+        else {
+            if (!checkVarExists(restOfLine)) return;
+            
+            //find in the map and print it
+            varValue v = variables[restOfLine];
+            if (v.type == TYPE_INT) cout << ">> " << v.i_val << endl;
+            else if (v.type == TYPE_FLOAT) cout << ">> " << v.f_val << endl;
+            else if (v.type == TYPE_STRING) cout << ">> " << v.s_val << endl;
+        }
+    }
+    else if (command == "GOTO") {
+        int targetLine;
+        ss >> targetLine;
+
+        //make sure the line exists in Buffer
+        if (targetLine > 0 && targetLine <= Buffer.size()) {
+            //fun trick here, target line - 1 to adjust for 0 index and
+            //then another -1 so that the ++i of loop runs the actual line required
+            i = targetLine - 2;
+        }
+        else {
+            cout << "Error: Line " << targetLine << " does not exist!" << endl;
+        }
+    }
+    else if (command == "IF") {
+        string leftSide, op, rightSide;
+        ss >> leftSide >> op >> rightSide;
+
+        float leftVal = getValue(leftSide);
+        float rightVal = getValue(rightSide);
+
+        bool conditionMet = false;
+        if (op == "==") conditionMet = (leftVal == rightVal);
+        else if (op == "!=") conditionMet = (leftVal != rightVal);
+        else if (op == "<") conditionMet = (leftVal < rightVal);
+        else if (op == ">") conditionMet = (leftVal > rightVal);
+        else if (op == "<=") conditionMet = (leftVal <= rightVal);
+        else if (op == ">=") conditionMet = (leftVal >= rightVal);
+        else {
+            cout << "Error: Unknown operator '" << op << "' in IF statement!" << endl;
+            return;
+        }
+
+        //if the condition is met, extract the rest of the line and run it
+        if (conditionMet) {
+            string restOfLine;
+            getline(ss, restOfLine);
+            restOfLine = trim(restOfLine);
+            
+            if (!restOfLine.empty()) {
+                executeLine(restOfLine, i, Buffer);
+            }
+        }
+    }
+    else {
+        //ARITHMETIC LOGIC
+        string targetVar = command;
+        string equalsSign;
+        ss >> equalsSign;
+
+        if (equalsSign == "=") {
+            if (!checkVarExists(targetVar)) return;
+
+            string expression;
+            //take the mathematical expression and pass it to doMaths
+            getline(ss, expression);
+            
+            float result = doMaths(expression);
+            
+            variables[targetVar].f_val = result;
+            variables[targetVar].i_val = (int)result;
+
+            if (result == (int)result) {
+                variables[targetVar].type = TYPE_INT;
+            } else {
+                variables[targetVar].type = TYPE_FLOAT;
+            }
+        }
+    }
+}
+
 
 int main(int argc, char* argv[]) {
 
@@ -172,7 +348,6 @@ int main(int argc, char* argv[]) {
     //we stop using the file as we already have all the code in the buffer
     file.close();
    
-
     //code execution loop
     for(int i = 0; i < Buffer.size(); ++i){
         
@@ -190,139 +365,18 @@ int main(int argc, char* argv[]) {
             isRunning = true; //start executing
             continue;
         }
-        else if (command == "END") {
-            break;//strop executing
-        }
 
         //skip to next line if not executing yet
         if (!isRunning) {
             continue;
         }
 
-        //COMMANDS logic
-        if (command == "DECLARE") {
-            string restOfLine;
-            getline(ss, restOfLine);
-            stringstream declStream(restOfLine);
-            string varName;
-            while (getline(declStream, varName, ',')) {
-                varName = trim(varName);
-                if (!varName.empty()) {
-                    if (variables.count(varName) > 0) {
-                        cout << "Error: Variable '" << varName << "' already declared!" << endl;
-                    } else {
-                        variables[varName] = varValue();
-                    }
-                }
-            }
-        }
-        else if (command == "INPUT") {
-            string varName;
-            ss >> varName;
-            
-            if (!checkVarExists(varName)) continue;
+        //pass line to the execution engine
+        executeLine(currentLine, i, Buffer);
 
-            cout << "Enter value for " << varName << ": ";
-            string rawInput;
-            getline(cin >> ws, rawInput);
-            
-            varValue newVal;
-
-            //checks if starts with quote for string
-            if (rawInput[0] == '"') {
-                newVal.type = TYPE_STRING;
-                size_t lastQuote = rawInput.find_last_of('"');
-                if (lastQuote != string::npos && lastQuote > 0) {
-                    newVal.s_val = rawInput.substr(1, lastQuote - 1);
-                } else {
-                    newVal.s_val = rawInput.substr(1);
-                }
-            }
-            //checks if it has a dot for float
-            else if (rawInput.find('.') != string::npos) {
-                newVal.type = TYPE_FLOAT;
-                try { newVal.f_val = stof(rawInput); } catch(...) { newVal.f_val = 0.0f; }
-                newVal.i_val = (int)newVal.f_val; 
-            }
-            //otherwise assumes its an integer
-            else {
-                newVal.type = TYPE_INT;
-                try { newVal.i_val = stoi(rawInput); } catch(...) { newVal.i_val = 0; }
-                newVal.f_val = (float)newVal.i_val; 
-            }
-            
-            //save it to map
-            variables[varName] = newVal;
-        }
-        else if (command == "OUTPUT") {
-            string restOfLine;
-            getline(ss, restOfLine);
-            restOfLine = trim(restOfLine); 
-
-            if (restOfLine.empty()) continue;
-
-            //if string literal
-            if (restOfLine[0] == '"') {
-                size_t lastQuote = restOfLine.find_last_of('"');
-                if (lastQuote != string::npos && lastQuote > 0) {
-                    cout << ">> " << restOfLine.substr(1, lastQuote - 1) << endl;
-                } else {
-                    cout << "Error: Missing closing quote in OUTPUT!" << endl;
-                }
-            }
-            //if raw number
-            else if (isdigit(restOfLine[0]) || (restOfLine[0] == '-' && restOfLine.length() > 1 && isdigit(restOfLine[1]))) {
-                cout << ">> " << restOfLine << endl;
-            }
-            //else must be a variable
-            else {
-                if (!checkVarExists(restOfLine)) continue;
-                
-                //find in the map and print it
-                varValue v = variables[restOfLine];
-                if (v.type == TYPE_INT) cout << ">> " << v.i_val << endl;
-                else if (v.type == TYPE_FLOAT) cout << ">> " << v.f_val << endl;
-                else if (v.type == TYPE_STRING) cout << ">> " << v.s_val << endl;
-            }
-        }
-        else if (command == "GOTO") {
-            int targetLine;
-            ss >> targetLine;
-
-            //make sure the line exists in Buffer
-            if (targetLine > 0 && targetLine <= Buffer.size()) {
-                //fun trick here, target line - 1 to adjust for 0 index and
-                //then another -1 so that the ++i of loop runs the actual line required
-                i = targetLine - 2;
-            }
-            else {
-                cout << "Error: Line " << targetLine << " does not exist!" << endl;
-            }
-        }
-        else {
-            //ARITHMETIC LOGIC
-            string targetVar = command;
-            string equalsSign;
-            ss >> equalsSign;
-
-            if (equalsSign == "=") {
-                if (!checkVarExists(targetVar)) continue;
-
-                string expression;
-                //take the mathematical expression and pass it to doMaths
-                getline(ss, expression);
-                
-                float result = doMaths(expression);
-                
-                variables[targetVar].f_val = result;
-                variables[targetVar].i_val = (int)result;
-
-                if (result == (int)result) {
-                    variables[targetVar].type = TYPE_INT;
-                } else {
-                    variables[targetVar].type = TYPE_FLOAT;
-                }
-            }
+        //break out completely if an END command was hit
+        if (!isRunning) {
+            break;
         }
     }
 
