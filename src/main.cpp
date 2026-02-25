4285bcc85ee5a8c1cfc1532c74c2d71417064970
omnimistic <prasparadise@gmail.com>
2026-02-25 23:01:00 +0530

    Added DECLARE keyword to explicitly declare a variable

--- src/main.cpp
+++ src/main.cpp
@@ -4,29 +4,39 @@
 #include <string>
 #include <sstream>
 #include <map>
+#include <cctype>
 
 using namespace std;
 
-// Language ka RAM that we use for storing variables
 map<string, int> variables;
 
 bool isRunning = false;
 
+string trim(const string& str) {
+    size_t first = str.find_first_not_of(" \t");
+    if (first == string::npos) return "";
+    size_t last = str.find_last_not_of(" \t");
+    return str.substr(first, (last - first + 1));
+}
+
+bool checkVarExists(const string& varName) {
+    if (variables.count(varName) == 0) {
+        cout << "Error: Undeclared variable '" << varName << "'!" << endl;
+        return false;
+    }
+    return true;
+}
 
 int doMaths(const string& expression) {
 
-    //local struct so that i can write functions inside a fucntion because cant do that by default in C++
     struct Parser {
 
-        //moves the pointer forward to ignore spaces and tabs
         void skipWhitespace(const string& str, size_t& pos) {
             while (pos < str.length() && isspace(str[pos])) {
                 pos++;
             }
         }
 
-        //reads a word, if digit, converts to integer, if letters, pulls the value from variables map
-        //also handles negative sign and restarts if it sees ( ).
         int parseFactor(const string& str, size_t& pos) {
             skipWhitespace(str, pos);
             if (pos >= str.length()) return 0;
@@ -56,12 +66,11 @@ int doMaths(const string& expression) {
             if (isdigit(token[0])) {
                 return sign * stoi(token);
             } else {
+                if (!checkVarExists(token)) return 0;
                 return sign * variables[token]; // Looks up the global variables map
             }
         }
         
-        //handles multiplication, division, and modulus
-        //makes sure these happen before addition or subtraction.
         int parseTerm(const string& str, size_t& pos) {
             int val = parseFactor(str, pos);
             while (true) {
@@ -87,8 +96,6 @@ int doMaths(const string& expression) {
             return val;
         }
         
-        //handles addition and subtraction.
-        //starting funciton of the parser
         int parseExpression(const string& str, size_t& pos) {
             int val = parseTerm(str, pos);
             while (true) {
@@ -170,10 +177,28 @@ int main(int argc, char* argv[]) {
         }
 
         //COMMANDS logic
-        if (command == "INPUT") {
+        if (command == "DECLARE") {
+            string restOfLine;
+            getline(ss, restOfLine);
+            stringstream declStream(restOfLine);
+            string varName;
+            while (getline(declStream, varName, ',')) {
+                varName = trim(varName);
+                if (!varName.empty()) {
+                    if (variables.count(varName) > 0) {
+                        cout << "Error: Variable '" << varName << "' already declared!" << endl;
+                    } else {
+                        variables[varName] = 0;
+                    }
+                }
+            }
+        }
+        else if (command == "INPUT") {
             string varName;
             ss >> varName;
             
+            if (!checkVarExists(varName)) continue;
+
             cout << "Enter value for " << varName << ": ";
             int value;
             cin >> value;
@@ -185,6 +210,8 @@ int main(int argc, char* argv[]) {
             string varName;
             ss >> varName;
             
+            if (!checkVarExists(varName)) continue;
+
             //find in the map and print it
             cout << ">> " << variables[varName] << endl;
         }
@@ -195,11 +222,11 @@ int main(int argc, char* argv[]) {
             ss >> equalsSign;
 
             if (equalsSign == "=") {
+                if (!checkVarExists(targetVar)) continue;
+
                 string expression;
-                // Grab the entire rest of the mathematical equation
-                getline(ss, expression); 
-                
-                // Pass it to the Do_maths function and save the answer!
+                //take the mathematical expression and pass it to doMaths
+                getline(ss, expression);
                 variables[targetVar] = doMaths(expression);
             }
         }
